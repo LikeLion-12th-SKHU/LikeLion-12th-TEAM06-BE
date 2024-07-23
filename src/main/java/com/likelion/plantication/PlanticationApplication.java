0e63e939fac48b92f9d764ab3b948713ddbb7a3c
Rommmu <tonty1222@naver.com>
2024-07-23 21:26:25 +0900

    🎉 Chore: 프로젝트 세팅 [#2]

---
+++ src/main/java/com/likelion/plantication/PlanticationApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.plantication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanticationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/plantication/PlanticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.plantication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
