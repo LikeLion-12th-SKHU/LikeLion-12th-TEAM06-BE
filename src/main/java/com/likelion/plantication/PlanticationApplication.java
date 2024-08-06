b6379fbd9dd3f6dbdef026e0e8b4bd451b517ef0
Rommmu <tonty1222@naver.com>
2024-08-06 21:25:39 +0900

    ✨ Feat: Swagger CORS

--- src/main/java/com/likelion/plantication/PlanticationApplication.java
+++ src/main/java/com/likelion/plantication/PlanticationApplication.java
@@ -1,8 +1,11 @@
 package com.likelion.plantication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = {@Server(url = "https://plantication.site", description = "도메인 설명")})
 @SpringBootApplication
 public class PlanticationApplication {
 
